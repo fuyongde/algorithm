4df64f6a38b6513bf4a9f83384c57593111d5d6f
fuyongde <fuyongde@foxmail.com>
2020-01-01 13:37:44 +0800

    :sparkles: leetcode 第155题，最小栈的问题

--- leetcode/src/main/java/com/fuyd/linkednode/Problems19.java
+++ leetcode/src/main/java/com/fuyd/linkednode/Problems19.java
@@ -2,16 +2,17 @@ package com.fuyd.linkednode;
 
 /**
  * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
- *
+ * <p>
  * 示例：
- *
+ * <p>
  * 给定一个链表: 1->2->3->4->5, 和 n = 2.
- *
+ * <p>
  * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * @author fuyongde
  * @date 2019/12/30
  */
