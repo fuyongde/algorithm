e98f1e1daef325181f5b6d52ea44f66d3f80dd4d
fuyongde <fuyongde@zhuifintech.com>
2020-01-05 22:24:44 +0800

    :sparkles: 二叉堆

---
+++ data-structure/src/test/java/com/fuyd/HeapTest.java
@@ -0,0 +1,27 @@
+package com.fuyd;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author fuyongde
+ * @date 2020/1/3
+ */
+public class HeapTest {
+
+    @Test
+    public void buildHeap() {
+        int[] items = {7, 1, 3, 10, 5, 2, 8, 9, 6};
+        Heap.buildHeap(items);
+        System.out.println(Arrays.toString(items));
+    }
+
+    @Test
+    public void upAdjust() {
+        int[] array = {1, 3, 2, 6, 5, 7, 8, 9, 10, 0};
+        Heap.upAdjust2(array);
+        // [0, 1, 2, 6, 3, 7, 8, 9, 10, 5]
+        System.out.println(Arrays.toString(array));
+    }
+}
