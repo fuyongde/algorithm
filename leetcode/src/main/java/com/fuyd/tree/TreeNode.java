9498266131d17eec06e044d735f0bbe0e37f21a4
fuyongde <fuyongde@foxmail.com>
2020-01-07 21:37:57 +0800

    :sparkles: 找出二叉树的最大深度

---
+++ leetcode/src/main/java/com/fuyd/tree/TreeNode.java
@@ -0,0 +1,15 @@
+package com.fuyd.tree;
+
+/**
+ * @author fuyongde
+ * @date 2020/1/7 21:27
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
