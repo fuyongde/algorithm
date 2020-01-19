53dfde58900e883705b419a4144eabcf1da248b9
fuyongde <fuyongde@foxmail.com>
2020-01-19 17:32:35 +0800

    :sparkles: leetcode 第58题

---
+++ leetcode/src/main/java/com/fuyd/other/Problems58.java
@@ -0,0 +1,59 @@
+package com.fuyd.other;
+
+import java.util.Objects;
+
+/**
+ * 给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。
+ * <p>
+ * 如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+ * <p>
+ * 如果不存在最后一个单词，请返回 0 。
+ * <p>
+ * 说明：一个单词是指仅由字母组成、不包含任何空格的 最大子字符串。
+ * <p>
+ *  
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: "Hello World"
+ * 输出: 5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/length-of-last-word
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author fuyongde
+ * @date 2020/1/19
+ */
+public class Problems58 {
+
+    public int lengthOfLastWord(String s) {
+        if (Objects.isNull(s) || s.length() == 0) {
+            return 0;
+        }
+        String[] array = s.split(" ");
+        return array.length > 0 ? array[array.length - 1].length() : 0;
+    }
+
+    public int lengthOfLastWord2(String s) {
+        int end = s.length() - 1;
+        while (end >= 0 && s.charAt(end) == ' ') {
+            end--;
+        }
+        if (end < 0) {
+            return 0;
+        }
+        int start = end;
+        while (start >= 0 && s.charAt(start) != ' ') {
+            start--;
+        }
+        return end - start;
+    }
+
+    public int lengthOfLastWord3(String s) {
+        s = s.trim();
+        int start = s.lastIndexOf(" ") + 1;
+        return s.substring(start).length();
+    }
+
+}
