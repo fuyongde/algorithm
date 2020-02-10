403258c981dc6e26c23364bf2a0faff9c6e812f9
fuyongde <fuyongde@foxmail.com>
2020-02-10 20:31:34 +0800

    :sparkles: LeetCode 第326题

---
+++ leetcode/src/main/java/com/fuyd/other/Solution326.java
@@ -0,0 +1,47 @@
+package com.fuyd.other;
+
+/**
+ * 给定一个整数，写一个函数来判断它是否是 3 的幂次方。
+ *
+ * 示例 1:
+ *
+ * 输入: 27
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: 0
+ * 输出: false
+ * 示例 3:
+ *
+ * 输入: 9
+ * 输出: true
+ * 示例 4:
+ *
+ * 输入: 45
+ * 输出: false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/power-of-three
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author fuyongde
+ * @date 2020/2/10 20:20
+ */
+public class Solution326 {
+
+    /**
+     * 循环迭代法
+     * 只要 n % 3 (余数为0，就一直将 n = n / 3)
+     * 时间复杂度：O(log(n))
+     * 空间复杂度：O(1)
+     */
+    public boolean isPowerOfThree(int n) {
+        if (n < 1) {
+            return false;
+        }
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
