d2e5014c1511cdb8b1292104c46ef3c2b9cd02c1
fuyongde <fuyongde@foxmail.com>
2020-02-14 21:49:38 +0800

    :sparkles: LeetCode 第1455题

---
+++ leetcode/src/main/java/com/fuyd/interview/Solution1455.java
@@ -0,0 +1,23 @@
+package com.fuyd.interview;
+
+/**
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "We are happy."
+ * 输出："We%20are%20happy."
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author fuyongde
+ * @date 2020/2/14 21:45
+ */
+public class Solution1455 {
+
+    public String replaceSpace(String s) {
+        return s.replaceAll(" ", "%20");
+    }
+}
