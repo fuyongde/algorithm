7d1ae4fd7f4a8e713dda56c18b07ce7f99d8a4ef
fuyongde <fuyongde@foxmail.com>
2020-02-19 11:23:08 +0800

    :sparkles: leetcode 第391题

---
+++ leetcode/src/main/java/com/fuyd/tree/Solution391.java
@@ -0,0 +1,61 @@
+package com.fuyd.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 107. 二叉树的层次遍历 II
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ *
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其自底向上的层次遍历为：
+ *
+ * [
+ *   [15,7],
+ *   [9,20],
+ *   [3]
+ * ]
+ *
+ * @author fuyongde
+ * @date 2020/2/19
+ */
+public class Solution391 {
+
+    /**
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     */
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int count = queue.size();
+            for (int i = 0; i < count; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            result.addFirst(list);
+        }
+        return result;
+    }
+}
