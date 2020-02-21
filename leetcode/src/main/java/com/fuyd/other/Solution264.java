f69e2ef84e237a24bef25ac3137e79ae83d7ab73
fuyongde <fuyongde@foxmail.com>
2020-02-21 15:31:11 +0800

    :sparkles: leetcode 第264题，新增三指针法。

--- leetcode/src/main/java/com/fuyd/other/Solution264.java
+++ leetcode/src/main/java/com/fuyd/other/Solution264.java
@@ -3,16 +3,16 @@ package com.fuyd.other;
 /**
  * 264. 丑数 II
  * 编写一个程序，找出第 n 个丑数。
- *
+ * <p>
  * 丑数就是只包含质因数 2, 3, 5 的正整数。
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 输入: n = 10
  * 输出: 12
  * 解释: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 是前 10 个丑数。
  * 说明:
- *
+ * <p>
  * 1 是丑数。
  * n 不超过1690。
  *
@@ -36,6 +36,31 @@ public class Solution264 {
             }
         }
         return -1;
+    }
+
+    /**
+     * 三指针法
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     */
+    public int nthUglyNumber2(int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int i2 = 0, i3 = 0, i5 = 0;
+        for (int i = 1; i < n; i++) {
+            int min = Math.min(dp[i2] * 2, Math.min(dp[i3] * 3, dp[i5] * 5));
+            if (min == dp[i2] * 2) {
+                i2++;
+            }
+            if (min == dp[i3] * 3) {
+                i3++;
+            }
+            if (min == dp[i5] * 5) {
+                i5++;
+            }
+            dp[i] = min;
+        }
+        return dp[n - 1];
     }
 
     public static boolean isUgly(int num) {
