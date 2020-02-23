42729f0abf0f76d7f1eb749f7f4adde01d10ed8e
fuyongde <fuyongde@foxmail.com>
2020-02-23 21:53:06 +0800

    :memo: Writing docs.

--- leetcode/src/main/java/com/fuyd/other/Solution88.java
+++ leetcode/src/main/java/com/fuyd/other/Solution88.java
@@ -26,6 +26,9 @@ import java.util.Arrays;
  */
 public class Solution88 {
 
+    /**
+     * 直接调用系统相关的 API
+     */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         System.arraycopy(nums2, 0, nums1, m, n);
         Arrays.sort(nums1);
